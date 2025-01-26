d0bd4dfaa40fbd9081755ace49bf1d4a9604a49f
mohamed-iheb <mohamed-iheb.kacem@ensta-paris.fr>
2025-01-26 16:02:05 +0100

    SP pb update

--- Pb_Robuste_V0.jl
+++ Pb_Robuste_V0.jl
@@ -1,9 +1,8 @@
 using JuMP
 using CPLEX
+using GLPK  # Pour résoudre le sous-problème comme un LP
 
-# Charger les données depuis le fichier
-include("data/instance_n5.txt")
-
+# Données du problème
 n = 5
 t = [
     0 260 864 263 374;
@@ -21,24 +20,24 @@ C = 6
 nodes = 1:n
 A = [(i, j) for i in nodes, j in nodes if i != j]
 
-# Modèle maître
+# Modèle principal
 model = Model(CPLEX.Optimizer)
 
 @variable(model, x[i in nodes, j in nodes], Bin)  # Variable binaire pour les arcs
 @variable(model, u[i in nodes] >= 0)  # Quantité livrée à chaque nœud
 @variable(model, z)  # Variable de robustesse
 
-# Fonction objectif (minimisation des coûts de tournée)
+# Fonction objectif
 @objective(model, Min, z)
 
-# Contraintes
+# Contraintes de tournée
 for i in nodes
     if i != 1
-        @constraint(model, sum(x[j, i] for j in nodes if j != i) == 1)  # Un véhicule arrive
-        @constraint(model, sum(x[i, j] for j in nodes if j != i) == 1)  # Un véhicule part
+        @constraint(model, sum(x[j, i] for j in nodes if j != i) == 1)
+        @constraint(model, sum(x[i, j] for j in nodes if j != i) == 1)
     end
 end
-@constraint(model, sum(x[1, j] for j in nodes if j != 1) == sum(x[j, 1] for j in nodes if j != 1))  # Retour au dépôt
+@constraint(model, sum(x[1, j] for j in nodes if j != 1) == sum(x[j, 1] for j in nodes if j != 1))
 
 # Contraintes de capacité
 for i in nodes
@@ -61,25 +60,31 @@ for j in nodes
     end
 end
 
-# Contraintes de robustesse pour chaque arc (i, j)
-for i in nodes, j in nodes
-    if i != j
-        @constraint(model, z >= t[i, j] * x[i, j])  # + th[i] * x[i, j]
-    end
-end
-
-# Définir le callback pour les plans coupants
+# Callback pour résoudre le sous-problème
 function cutting_plane_callback(cb_data)
-    # Obtenez les valeurs actuelles des variables
+    # Obtenez les valeurs actuelles de x
     x_val = callback_value.(cb_data, x)
     z_val = callback_value(cb_data, z)
 
-    # Ajoutez une coupe si nécessaire
-    for (i, j) in A
-        t_robust = t[i, j] + th[i] + th[j]
-        if z_val < t_robust * x_val[i, j]
-            # Ajout de la coupe via l'interface de CPLEX
-            cb_data.add_lazy_constraint(z >= t_robust * x[i, j])
+    # Résolution du sous-problème
+    SP = Model(GLPK.Optimizer)
+    @variable(SP, 0 <= delta1[i in nodes, j in nodes] <= 1)
+    @variable(SP, 0 <= delta2[i in nodes, j in nodes] <= 2)
+
+    @objective(SP, Max, sum((delta1[i, j] * (th[i] + th[j]) + delta2[i, j] * th[i] * th[j]) * x_val[i, j] for (i, j) in A))
+    @constraint(SP, sum(delta1[i, j] for (i, j) in A) <= T)
+    @constraint(SP, sum(delta2[i, j] for (i, j) in A) <= T^2)
+
+    optimize!(SP)
+
+    if termination_status(SP) == MOI.OPTIMAL
+        z_star = objective_value(SP)
+        delta1_star = value.(delta1)
+        delta2_star = value.(delta2)
+
+        if z_star > z_val
+            z_rhs = sum((delta1_star[i, j] * (th[i] + th[j]) + delta2_star[i, j] * th[i] * th[j]) * x[i, j] for (i, j) in A)
+            MOI.submit(cb_data, MOI.LazyConstraint(), z >= z_rhs)
         end
     end
 end
@@ -90,17 +95,17 @@ MOI.set(model, MOI.LazyConstraintCallback(), cutting_plane_callback)
 # Résolution
 optimize!(model)
 
-# Vérifiez si une solution faisable a été trouvée
+# Résultats
 if termination_status(model) in [MOI.OPTIMAL, MOI.FEASIBLE]
     println("Objective value (coût total) : ", objective_value(model))
-    println("x values (arcs choisis) :")
+    println("Solutions des arcs sélectionnés :")
     for i in nodes, j in nodes
         if value(x[i, j]) > 0.5
             println("Arc ($i -> $j)")
         end
     end
-    println("u values (quantité livrée) : ", value.(u))
-    println("z value (valeur robuste) : ", value(z))
+    println("Quantité livrée : ", value.(u))
+    println("Valeur robuste : ", value(z))
 else
     println("Aucune solution faisable trouvée. Statut : ", termination_status(model))
 end
