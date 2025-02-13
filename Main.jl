aa8959bfdda1e21cbe0006d7c8bb3767b6a63d35
mohamed-iheb <mohamed-iheb.kacem@ensta-paris.fr>
2025-02-13 21:51:13 +0100

    performances

--- Main.jl
+++ Main.jl
@@ -6,6 +6,7 @@ include("Pb_Statique_V0.jl")
 include("LazyCallback.jl")
 include("Dualisation.jl")
 include("heuristique.jl")
+include("PlansCoupants.jl")
 
 
 objectif_statique_n5 = 2561
@@ -18,6 +19,10 @@ objectif_robuste_non_euclidien_euclidien = [[3307, 2066, 3270, 3113, 2684],[3087
 timeS=[]
 timeD=[]
 timeL=[]
+timeP=[]
+timeH=[]
+valH=[]
+valP=[]
 valS=[]
 valD=[]
 valL=[]
@@ -42,7 +47,17 @@ for E in ["false", "true"]
         t1=Dates.now()
         vD= Resolution_Dualisation(n,d,t,C,th,T)
         t2=Dates.now()
-        push!(timeD, t2 - t1) 
+        push!(timeD, t2 - t1)
+        
+        t1=Dates.now()
+        vH= heuristique(n,d,t,C,th,T)
+        t2=Dates.now()
+        push!(timeH, t2 - t1) 
+
+        t1=Dates.now()
+        vP= PlC(n,d,t,C,th,T)
+        t2=Dates.now()
+        push!(timeP, t2 - t1) 
 
         t1=Dates.now()
         vL= Resolution_LCb(n,d,t,C,th)
@@ -51,77 +66,83 @@ for E in ["false", "true"]
 
         push!(valD, vD)  
         push!(valL, vL)  
-        push!(valS, v)  
+        push!(valS, v)
+        push!(valP, vP)  
+        push!(valH, vH)  
         push!(P, PR) 
     end
 end
-println(valS)
-println(valD)
-println(valL)
-println(timeS)
-println(timeL)
-println(timeD)
+println("valS  :",valS)
+println("valD  :",valD)
+println("valL  :",valL)
+println("valH  :",valH)
+println("valP  :",valP)
 println(P)
-
+#GH= (valH - objectif_robuste_non_euclidien_euclidien) 
+println("GH =", 100*(valH -[3307, 2066, 3270, 3113, 2684,3087, 3132, 3776, 4181, 5423])/[3307, 2066, 3270, 3113, 2684,3087, 3132, 3776, 4181, 5423])
+println("G00H =", objectif_robuste_non_euclidien_euclidien)
 # Conversion en secondes
 timeS = [t.value / 1000 for t in timeS]  
 timeD = [t.value / 1000 for t in timeD]
 timeL = [t.value / 1000 for t in timeL]
+timeP = [t.value / 1000 for t in timeP]
+timeH = [t.value / 1000 for t in timeH]
 
 println("timeS =",timeS)
 println("timeL =",timeL)
 println("timeD =",timeD)
-
+println("timeP =",timeP)
+println("timeH =",timeH)
 
 GtimeS=[]
 GtimeD=[]
 GtimeL=[]
+GtimeP=[]
+GtimeH=[]
 
-dir_path = "data/"
+
+dir_path = "data1/"
 for file in readdir(dir_path)
     file_path = joinpath(dir_path, file)
     println("___________________________________________")
     include(file_path)
 
-    # Résolution MTZ avec timeout de 1s
-    t1 = Dates.now()
-    task = @spawn Resolution_MTZ(n, d, t, C)
-    try
-        v = fetch(task, 1)  # Attend max 1s
-    catch
-        println("Timeout pour Resolution_MTZ")
-    end
-    t2 = Dates.now()
-    push!(GtimeS, t2 - t1)
+    t1=Dates.now()
+    v= Resolution_MTZ(n, d, t, C)
+    t2=Dates.now()
+    push!(GtimeS, t2 - t1) 
 
-    # Résolution Dualisation avec timeout de 1s
-    t1 = Dates.now()
-    task = @spawn Resolution_Dualisation(n, d, t, C, th, T)
-    try
-        vD = fetch(task, 1)  # Attend max 1s
-    catch
-        println("Timeout pour Resolution_Dualisation")
-    end
-    t2 = Dates.now()
+    t1=Dates.now()
+    vD= Resolution_Dualisation(n,d,t,C,th,T)
+    t2=Dates.now()
     push!(GtimeD, t2 - t1)
+    
+    t1=Dates.now()
+    vH= heuristique(n,d,t,C,th,T)
+    t2=Dates.now()
+    push!(GtimeH, t2 - t1) 
 
-    # Résolution LCb avec timeout de 1s
-    t1 = Dates.now()
-    task = @spawn Resolution_LCb(n, d, t, C, th)
-    try
-        vL = fetch(task, 1)  # Attend max 1s
-    catch
-        println("Timeout pour Resolution_LCb")
-    end
-    t2 = Dates.now()
-    push!(GtimeL, t2 - t1)
+    t1=Dates.now()
+    vP= PlC(n,d,t,C,th,T)
+    t2=Dates.now()
+    push!(GtimeP, t2 - t1) 
+
+    t1=Dates.now()
+    vL= Resolution_LCb(n,d,t,C,th)
+    t2=Dates.now()
+    push!(GtimeL, t2 - t1) 
+
 end
 
 
 GtimeS = [t.value / 1000 for t in GtimeS]  
 GtimeD = [t.value / 1000 for t in GtimeD]
 GtimeL = [t.value / 1000 for t in GtimeL]
+GtimeP = [t.value / 1000 for t in GtimeP]
+GtimeH = [t.value / 1000 for t in GtimeH]
 
 println("timeS =",GtimeS)
 println("timeL =",GtimeL)
 println("timeD =",GtimeD)
+println("timeP =",GtimeP)
+println("timeH =",GtimeH)
